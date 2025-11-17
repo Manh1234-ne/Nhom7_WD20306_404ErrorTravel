4a47f73dab34b85d1c1a729bb30f78f87344394c
LZC-06a <laviss2k6tx@gmail.com>
2025-11-17 14:25:01 +0700

    a

--- BaseExam/controllers/HomeController.php
+++ BaseExam/controllers/HomeController.php
@@ -1,4 +1,3 @@
-<<<<<<< HEAD:BaseExam/BaseExam/controllers/HomeController.php
 <?php
 require_once __DIR__ . '/../configs/env.php';
 class HomeController
@@ -9,13 +8,3 @@ class HomeController
     }
 }
  
-=======
-<?php
-require_once __DIR__ . '/../configs/env.php';
-
-class HomeController {
-    public function index() {
-        require PATH_VIEW . 'home.php';
-    }
-};
->>>>>>> e789d3ef2f7881f853c1c57bed64ccf716f8ea61:BaseExam/controllers/HomeController.php
